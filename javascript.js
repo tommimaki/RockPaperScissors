1cabe9c06cc12440bc871a1dd58f197447fd3724
tommibish <74586216+tommimaki@users.noreply.github.com>
2022-11-29 22:19:09 -0500

    mvp

--- javascript.js
+++ javascript.js
@@ -1,2 +1,51 @@
-console.log("test")
 
+//variables
+let compInput;
+let userInput = prompt("write rock paper or scissors");
+
+function getCompuperChoice() {
+
+  let choice = Math.floor(Math.random() * 3);
+
+  if (choice == 0) {
+    compInput = "rock";
+  } else if (choice == 1) {
+    compInput = "paper";
+  } else {
+    compInput = "scissors"
+  }
+}
+
+getCompuperChoice();
+console.log(compInput)
+console.log(userInput)
+
+function compareAnswers(compInput, userInput){
+    if (compInput == userInput) {
+        console.log('tie')
+
+        //rock choises
+    } else if (compInput == "rock" && userInput == "paper"){
+        console.log(`computer played ${compInput} user played ${userInput}, user wins `)
+    } else if (compInput == "rock" && userInput == "scissors"){
+        console.log(`computer played ${compInput} user played ${userInput}, computer wins `)
+    }  
+    
+    // paper choises
+    else if (compInput == "paper" && userInput == "scissors"){
+        console.log(`computer played ${compInput} user played ${userInput}, user wins `)
+    } else if (compInput == "paper" && userInput == "rock"){
+        console.log(`computer played ${compInput} user played ${userInput}, computer wins `)
+    }  
+     // scissors choises
+    else if (compInput == "scissors" && userInput == "rock"){
+        console.log(`computer played ${compInput} user played ${userInput}, user wins `)
+    } else if (compInput == "scissors" && userInput == "paper"){
+        console.log(`computer played ${compInput} user played ${userInput}, computer wins `)
+    } else{
+        console.log('input not valid');
+    }
+    
+}
+
+compareAnswers(compInput,userInput);
