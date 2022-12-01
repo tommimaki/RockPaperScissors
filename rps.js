12988c21e364098e52a461ec29b5e1c8fd10c4c3
tommibish <74586216+tommimaki@users.noreply.github.com>
2022-11-30 22:47:16 -0500

    finished console game

--- rps.js
+++ rps.js
@@ -1,53 +1,57 @@
 // vars
 let computerHand;
 
-
 // counters
 let userPoints = 0;
 let computerPoints = 0;
 
 // get computer answer function
-//lets add an array where we have written the possible answers
-// and random number chooses an option
+// array with possible answers, Math.random chooses one
 
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   return (computerHand = choices[Math.floor(Math.random() * choices.length)]);
 }
-//testi, pelaa pelaa
-//  getComputerChoice();
-// console.log(computerHand)
 
-// seuraava funktio, vertaa computerHand ja userHand
-
+ // comparing answers
 function comparingAnswers(userHand) {
   let computer = getComputerChoice();
   let rounds = 0;
 
-  console.log("user plays: " + userHand + " computer plays: " + computer);
   if (userHand == computer) {
-    return "tie";
+    return `tie, both played ${userHand}`;
   } else if (
     (computer == "paper" && userHand == "scissors") ||
     (computer == "scissors" && userHand == "rock") ||
     (computer == "rock" && userHand == "paper")
   ) {
     userPoints = userPoints + 1;
-    return `user wins with ${userHand}, score: user: ${userPoints}, computer: ${computerPoints}`;
+    return `user wins with ${userHand} against ${computer}, score: user: ${userPoints}, computer: ${computerPoints}`;
   } else {
     computerPoints = computerPoints + 1;
-    return `computer wins with ${computer}, score: user: ${userPoints}, computer: ${computerPoints}`;
+    return `computer wins with ${computer} against ${userHand}, score: user: ${userPoints}, computer: ${computerPoints}`;
   }
 }
 
 // function to play 5 rounds and announce winner
 
 function play5() {
-    let x;
-  for (let i = 0; i < 5; i++) {
-    x = prompt("play: rock, paper or scissors");
-    console.log(comparingAnswers(x));
-  } 
+  let x;
+  for (let i = 0; i <= 5; i++) {
+    userInput = prompt("play: rock, paper or scissors").toLowerCase();
+    console.log(comparingAnswers(userInput));
+
+    if (i == 4) {
+      if (computerPoints > userPoints) {
+        return `Computer wins the game with ${computerPoints} points :(!  refresh page to play again)`;
+      } else if (computerPoints < userPoints) {
+        return `user wins the game with ${userPoints} points!!`;
+      } else {
+        return `tie, userpoints: ${userPoints},  computerpoints: ${computerPoints}, refresh to play again `;
+      }
+    }
+  }
 }
 
-play5();
+// call function and log to console
+console.log(play5());
